8e929be9fe26a5cd073149dd7cf72b15496f83fe
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-21 22:22:18 +0700

    Fix Forge packet test import

--- forge-1.20.1/src/test/java/com/nstut/forge/test/MarketPacketTest.java
+++ forge-1.20.1/src/test/java/com/nstut/forge/test/MarketPacketTest.java
@@ -6,6 +6,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
